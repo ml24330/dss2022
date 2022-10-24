43ac3edd61d259136942ad95cbae755aae29aa32
ml <m.liu48@lse.ac.uk>
2022-10-24 14:55:28 +0100

    add structure

--- Meeting 1/scraper.py
+++ Meeting 1/scraper.py
@@ -1,14 +1,14 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-# chrome_options.headless = True
+chrome_options.headless = True
 driver = webdriver.Chrome(options=chrome_options)
 
 start_url = "https://www.wikipedia.org"
 driver.get(start_url)
 
 search_el = driver.find_element("name", "search")
-search_el.send_keys("test")
+search_el.send_keys("War")
 
 driver.find_element_by_css_selector('button[type=submit]').click()
 

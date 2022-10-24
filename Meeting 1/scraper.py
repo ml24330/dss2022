28151f312b60f39916aef933680090ad6affbb72
ml <m.liu48@lse.ac.uk>
2022-10-24 14:45:31 +0100

    first commit

---
+++ Meeting 1/scraper.py
@@ -0,0 +1,17 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+# chrome_options.headless = True
+driver = webdriver.Chrome(options=chrome_options)
+
+start_url = "https://www.wikipedia.org"
+driver.get(start_url)
+
+search_el = driver.find_element("name", "search")
+search_el.send_keys("test")
+
+driver.find_element_by_css_selector('button[type=submit]').click()
+
+driver.save_screenshot("image.png")
+
+driver.quit()
